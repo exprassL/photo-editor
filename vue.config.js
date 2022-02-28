a81522a0447da77e742eaa0bb7efacfc1b5bd013
exprassL <exprass@hotmail.com>
2022-02-28 20:46:02 +0800

    a new build, adding '/dist' to pulbicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/photo-editor' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/photo-editor/dist' : '/',
 
     outputDir: 'dist',
 
