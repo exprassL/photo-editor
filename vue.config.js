b55d2bb8ae96097f0d06f04506d0d90190299872
exprassL <exprass@hotmail.com>
2022-02-28 14:36:59 +0800

    add app.config.js

---
+++ vue.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/photo-editor' : '/',
+
+    outputDir: 'dist',
+
+    assetsDir: 'assets',
+
+    indexPath: 'index.html',
+
+    filenameHashing: true, // 默认：true
+
+    runtimeCompiler: false,
+
+    productionSourceMap: true,
+
+    crossorigin: undefined,
+
+    integrity: false,
+
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.mjs$/,
+                    include: /node_modules/,
+                    type: 'javascript/auto'
+                }
+            ]
+        }
+    },
+
+    devServer: {
+        // open: true, // 启动后自动打开浏览器
+    },
+
+    parallel: require('os').cpus().length > 1, // 默认： 系统 cpu 多于一个内核时自动启用
+
+    pwa: {},
+
+};
